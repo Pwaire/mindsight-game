ea8f52a31b9a85dc5345f1f652107c670ec6d5d5
Pwaire <60104454+Pwaire@users.noreply.github.com>
2025-05-29 17:47:42 -0400

    Remove downloadable file

--- main.js
+++ main.js
@@ -129,17 +129,6 @@ function saveTrainingResult() {
         }))
     };
 
-    const json = JSON.stringify(data, null, 2);
-    const blob = new Blob([json], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = `training_result_${Date.now()}.json`;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-
     window.parent.postMessage({ type: 'GAME_RESULT', result: data }, '*');
 }
 

84affd0a1270e82e830656674156b42543848ccc
Pwaire <60104454+Pwaire@users.noreply.github.com>
2025-07-16 16:46:03 -0400

    Fix tutorial display and centering

--- main.js
+++ main.js
@@ -200,6 +200,7 @@ function showTutorial() {
   setCalibrateContainerActive(false);
   setCalibrateButtonActive(false);
   setElementActive(tutorialContainer, true);
+  tutorialContainer.style.display = 'flex';
 
   AudioManager.play(explainRulesAudio);
 
